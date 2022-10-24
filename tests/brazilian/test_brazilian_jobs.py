c9628f12917865aeca52967a81b04118442a7586
Enei <eneipereira@outlook.com.br>
2022-10-24 20:31:18 -0300

    test: Implementa um teste para a função "read_brazilian_file", que lê um documento em português e retorna uma lista de dicionário, mas com as chaves em inglês.

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
+
+PATH = "tests/mocks/brazilians_jobs.csv"
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_BR = read_brazilian_file(PATH)
+
+    for job in jobs_BR:
+        assert "title" in job and "titulo" not in job
+        assert "salary" in job and "salario" not in job
+        assert "type" in job and "tipo" not in job
